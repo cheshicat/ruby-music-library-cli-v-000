c9149b78ed5fcdacac0b6ec48982929e0b25099e
Allison Howard <alliemhow@gmail.com>
2018-12-18 23:51:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,12 +10,18 @@ class Song
     @@all
   end
 
-  def self.destroy_all
-    self.all.clear
-  end
-
   def save
     self.class.all << self
   end
 
+  def self.create
+    
+  end
+
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  
 end
