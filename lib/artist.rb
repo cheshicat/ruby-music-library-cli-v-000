b2aa1d6fd1ff3af36262e9164a406f119f76b7c3
Allison Howard <alliemhow@gmail.com>
2018-12-18 17:35:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,4 @@
-class Song
-  attr_accessor :name
-  @@all = []
+class Artist
 
-  def initialize(name)
-    @name = name
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    self.all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    song = self.new(name)
-    song.save
-    song
-  end
 
 end
