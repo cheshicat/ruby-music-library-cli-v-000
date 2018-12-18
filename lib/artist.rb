0f06a9b32682040587ba36fbc4036ef0801f5373
Allison Howard <alliemhow@gmail.com>
2018-12-18 18:09:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,12 +25,14 @@ class Artist
     artist
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
     @songs << song
     song.artist == self
   end
+
+  def songs
+    @songs
+  end
+
+
 end
